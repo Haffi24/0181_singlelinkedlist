fab02045484f926d09b21fde28818cb8cd80f361
Haffi24 <haffisaefulloh@gmail.com>
2025-05-08 11:32:49 +0700

    membuat boolean delnode

--- file1.cpp
+++ file1.cpp
@@ -74,4 +74,28 @@ bool Search(int nim, node **previous, node **current )
     *current = START;
 
 
-    
+    while ((*current != NULL) && (nim != ( *current)->noMhs))
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+
+    return (*current != NULL);
+}
+
+bool delnode (int nim)
+{
+    node *current, *previous ;
+    if (!Search(nim, &previous, &current))
+    return false;
+
+    if (current == START)
+    START = START->next;
+    else
+
+        previous->next = current->next;
+
+    delete current;
+    return true;
+}
+
